c083b3041366486d300c0207abbf09ded01ac72f
rhian-cs <rhian.luis.cs+github@gmail.com>
2024-03-07 11:44:48 -0300

    Update repo script

--- create-sample-git-repo.sh
+++ create-sample-git-repo.sh
@@ -14,7 +14,6 @@ git init
 git config user.email "git-user@example.org"
 git config user.name "git-user"
 
-
 cat > file1.txt <<EOL
 line 1, Here's the next
 line 2,
@@ -32,5 +31,14 @@ EOL
 
 git add .
 git commit -m "Update file1.txt"
+
+cat > file1.txt <<EOL
+line 1, Here's the next
+line 2, BIG
+line 4, thing
+EOL
+
+git add .
+git commit -m "Add line 4 to file1.txt"
 
 echo "Script has completed running."
